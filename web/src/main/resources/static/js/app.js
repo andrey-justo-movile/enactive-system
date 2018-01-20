d3b87a82e7020e398c93eac6a4490ce0ae70d5d5
Andrey Justo <andrey.justo@gmail.com>
2018-01-20 19:53:10 +0000

    fix js

--- web/src/main/resources/static/js/app.js
+++ web/src/main/resources/static/js/app.js
@@ -47,7 +47,7 @@
         }
         
         $scope.signUp = function() {
-        	if ($scope.registerPassword != $scope.registerConfirmPassword || !$scope.registerEmail.$valid) {
+        	if ($scope.registerPassword != $scope.registerConfirmPassword) {
         		// TODO warn message
         		return;
         	}
