ed0317dda9fae47a1739bc1f5e5391c98f04aca1
Andrey Justo <andrey.justo@gmail.com>
2018-02-13 05:23:41 +0000

    async

--- web/src/main/java/com/social/enactive/bot/main/SocialEnactiveWebMain.java
+++ web/src/main/java/com/social/enactive/bot/main/SocialEnactiveWebMain.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
@@ -23,6 +26,14 @@ public class SocialEnactiveWebMain {
 
     public static ApplicationContext context;
 
+    @Bean
+    public ServletRegistrationBean dispatcherServlet() {
+      ServletRegistrationBean registration = new ServletRegistrationBean(
+              new DispatcherServlet(), "/");
+      registration.setAsyncSupported(true);
+      return registration;
+    }
+    
     public static void main(String[] args) throws InterruptedException {
         context = new SpringApplicationBuilder(SocialEnactiveWebMain.class).bannerMode(Banner.Mode.OFF).run(args);
     }
