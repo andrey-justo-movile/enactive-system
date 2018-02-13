530652179106a7a699d6bd5c360f302060df3037
Andrey Justo <andrey.justo@gmail.com>
2018-02-13 04:13:33 +0000

    rollback

--- web/src/main/java/com/social/enactive/bot/queue/handler/MessagingReceiverHandlerRouter.java
+++ web/src/main/java/com/social/enactive/bot/queue/handler/MessagingReceiverHandlerRouter.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -20,8 +21,8 @@ public class MessagingReceiverHandlerRouter {
 		this.webSocket = webSocket;
 	}
 
-	public void handleMessage(String rawObj) {
-		List<Message> messages = JacksonMapper.standardMapper().readJson(rawObj, listMessageRef);
+	public void handleMessage(@Payload List<Message> messages) {
+		//List<Message> messages = JacksonMapper.standardMapper().readJson(rawObj, listMessageRef);
 		Log.SYSTEM.info("Sending messages={}", messages);
 		Map<String, List<Message>> grouppedMessages = messages.stream().collect(Collectors.groupingBy(Message::getConversationId));
 		grouppedMessages.forEach((key, value) -> {
