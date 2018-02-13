1de9fe62eb973e746403c307898301bbeb7f874e
Andrey Justo <andrey.justo@gmail.com>
2018-02-13 05:14:34 +0000

    use raw string inside rabbit messages

--- engine/src/main/java/com/social/enactive/bot/queue/handler/MessagingHandler.java
+++ engine/src/main/java/com/social/enactive/bot/queue/handler/MessagingHandler.java
@@ -6,6 +6,7 @@ import com.social.enactive.bot.components.conversation.ConversationService;
 import com.social.enactive.bot.components.message.Message;
 import com.social.enactive.bot.components.message.MessageService;
 import com.social.enactive.bot.configuration.log.Log;
+import com.social.enactive.bot.configuration.mapper.JacksonMapper;
 import com.social.enactive.bot.engine.Engine;
 
 public class MessagingHandler {
@@ -27,7 +28,8 @@ public class MessagingHandler {
 	public void handleMessage(Message message) {
 		Log.SYSTEM.info("Message received={}", message);
 		try {
-			rabbitTemplate.convertAndSend(messageDeliver, message.getConversationId(), engine.process(conversationService.find(message.getConversationId()), message));
+			String rawMessages = JacksonMapper.standardMapper().toJson(engine.process(conversationService.find(message.getConversationId()), message));
+			rabbitTemplate.convertAndSend(messageDeliver, message.getConversationId(), rawMessages);
 		} catch (Exception e) {
 			Log.EXCEPTION.error("Couldn't send message", e);
 		} finally {
--- web/src/main/java/com/social/enactive/bot/queue/handler/MessagingReceiverHandlerRouter.java
+++ web/src/main/java/com/social/enactive/bot/queue/handler/MessagingReceiverHandlerRouter.java
@@ -6,18 +6,22 @@ import java.util.stream.Collectors;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.social.enactive.bot.components.message.Message;
 import com.social.enactive.bot.configuration.log.Log;
+import com.social.enactive.bot.configuration.mapper.JacksonMapper;
 
 public class MessagingReceiverHandlerRouter {
 
 	private final SimpMessagingTemplate webSocket;
+	private final TypeReference<List<Message>> listMessageRef = new TypeReference<List<Message>>() {};
 
 	public MessagingReceiverHandlerRouter(SimpMessagingTemplate webSocket) {
 		this.webSocket = webSocket;
 	}
 
-	public void handleMessage(List<Message> messages) {
+	public void handleMessage(String rawMessages) {
+		List<Message> messages = JacksonMapper.standardMapper().readJson(rawMessages, listMessageRef);
 		Log.SYSTEM.info("Sending messages={}", messages);
 		Map<String, List<Message>> grouppedMessages = messages.stream().collect(Collectors.groupingBy(Message::getConversationId));
 		grouppedMessages.forEach((key, value) -> {
